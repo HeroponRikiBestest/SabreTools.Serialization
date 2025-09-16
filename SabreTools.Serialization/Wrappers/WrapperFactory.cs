727bca901156b1f76c73bdea67a4f96f75db28e3
HeroponRikiBestest <jojloj3@gmail.com>
2025-09-16 17:18:07 -0400

    Made Changes

--- SabreTools.Serialization/Wrappers/WrapperFactory.cs
+++ SabreTools.Serialization/Wrappers/WrapperFactory.cs
@@ -301,7 +301,8 @@ namespace SabreTools.Serialization.Wrappers
             // TODO: Use constants from Models here
             #region GZip
 
-            if (magic.StartsWith([Models.GZIP.Constants.ID1, Models.GZIP.Constants.ID2]))
+
+            if (magic.StartsWith((byte[])[Models.GZIP.Constants.ID1, Models.GZIP.Constants.ID2]))
                 return WrapperType.GZip;
 
             if (extension.Equals("gz", StringComparison.OrdinalIgnoreCase))
