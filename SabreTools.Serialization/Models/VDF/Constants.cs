b5d9bd1d8ca4a3fc177817fbb777f0e9ae98007f
HeroponRikiBestest <jojloj3@gmail.com>
2026-01-06 10:55:59 -0700

    Don't parse constant strings

--- SabreTools.Serialization/Models/VDF/Constants.cs
+++ SabreTools.Serialization/Models/VDF/Constants.cs
@@ -4,10 +4,10 @@ namespace SabreTools.Data.Models.VDF
     {
         public static readonly byte[] Steam2SisSignatureBytes = [0x22, 0x53, 0x4B, 0x55, 0x22]; // "SKU"
 
-        public static readonly string Steam2SisSignatureString = System.Text.Encoding.ASCII.GetString(Steam2SisSignatureBytes);
+        public static readonly string Steam2SisSignatureString = "\"SKU\"";
 
         public static readonly byte[] Steam3SisSignatureBytes = [0x22, 0x73, 0x6B, 0x75, 0x22]; // "sku"
 
-        public static readonly string Steam3SisSignatureString = System.Text.Encoding.ASCII.GetString(Steam3SisSignatureBytes);
+        public static readonly string Steam3SisSignatureString = "\"sku\"";
     }
 }
